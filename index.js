cd50cc7f8e201839bf0a73be28792001339fcf06
Karthikeya Kona <konakarthik12@gmail.com>
2021-02-22 17:34:43 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const {WebhookClient} = require("discord.js");
+const path = require("path");
+const readline = require("readline");
+const Axios = require("axios");
+
+const {spawn} = require("child_process");
+
+const axios = Axios.create({
+    baseURL: process.env.DEV ? "http://localhost:8080" : "https://limelight-bot.herokuapp.com"
+})
+const ls = spawn('java', ["-Xms1G", "-Xmx3G", "-jar", "server.jar", "nogui"], {cwd: path.join(__dirname, "../minecraft")});
+ls.on('close', (code) => console.log(`child process exited with code ${code}`));
+ls.stderr.on('data', console.error);
+
+async function webhook() {
+    const {data} = await axios.get("/")
+    const webhookClient = new WebhookClient('813534901386215424', 'GzrN9PBUVbQzGJ-BRSV1kwsyDNcHjshPMZ9rPgOVec4V5QSnREYko3lhFY-IYRXXyzNj');
+
+    function sendMessage(userData, parts) {
+        webhookClient.send(parts.join(' '), userData);
+    }
+
+    function handleMessage(message) {
+        const [username, ...parts] = message.split(' ')
+        if (data[username]) {
+            sendMessage(data[username], parts)
+        }
+    }
+
+    const rl = readline.createInterface({input: ls.stdout});
+    rl.on('line', line => {
+        let message = line.toString().split(":").slice(3).join("").slice(1);
+        console.log(message)
+        handleMessage(message)
+    })
+
+}
+
+try {
+    webhook()
+} catch (e) {
+    console.error("Unable to start webhook client");
+    console.error(e)
+}
